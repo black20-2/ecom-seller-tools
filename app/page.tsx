bb80cad6094115a409d3e9cca039cdd800dca2c9
Faizan <blackpanther@gmail.com>
2026-08-10 22:43:13 +0500

    Fix page imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from "./components/Hero";
-import CalculatorExplorer from "./components/CalculatorExplorer";
-import CalculatorCategories from "./components/CalculatorCategories";
-import HowItWorks from "./components/HowItWorks";
-import FAQ from "./components/FAQ";
+import Hero from "@/components/Hero";
+import CalculatorExplorer from "@/components/CalculatorExplorer";
+import CalculatorCategories from "@/components/CalculatorCategories";
+import HowItWorks from "@/components/HowItWorks";
+import FAQ from "@/components/FAQ";
 
 export default function HomePage() {
   return (
